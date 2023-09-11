72a090c499d64d4a358ef5a4982ae16a50d5c9d8
Filip Spelina <spelifil@fit.cvut.cz>
2023-09-11 09:37:59 +0800

    v2: switch to python

---
+++ diaSorter.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+ahoj = "Hello"
+svet = "World"
+cislo = "2"
+cisloint = 2
+print(ahoj + " " + svet + " " + cislo)
+print(ahoj, svet, cislo, cisloint)
+
+testbool = False
+
+if testbool:
+    print("Tadaaa")
+else:
+    print("sadface:(")
+
+testrange = range(-2, 10)
+print(testrange)
+
+for i in testrange:
+    print(i, end=" ")
