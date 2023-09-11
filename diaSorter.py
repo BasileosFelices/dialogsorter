dc31e0de4575dbf980499f084d4d5e38712f8960
Filip Spelina <spelifil@fit.cvut.cz>
2023-09-11 23:03:19 +0800

    v2: person implementation

--- diaSorter.py
+++ diaSorter.py
@@ -1,21 +1,62 @@
-print("Hello World")
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+import datetime
+import configparser
 
-ahoj = "Hello"
-svet = "World"
-cislo = "2"
-cisloint = 2
-print(ahoj + " " + svet + " " + cislo)
-print(ahoj, svet, cislo, cisloint)
+class TextModeler:
+    def __new__(cls):
+        raise TypeError("TextModeler is a static class used for holding functions for string editing. It cannot be instanced")
+    
+    @staticmethod
+    def cleanSpaces(string):
+        return " ".join(string.split())
+    
+    # @staticmethod
+    # def readTime(string):
+        
+    #     date = str(string.split(" ")[0])
+    #     year = date.split("-")[0]
+    #     month = date.split("-")[1]
+    #     day = date.split("-")[2]
 
-testbool = False
+    #     clock = string.split(" ")[1]
+    #     hour = time.split(":")[0]
+    #     minute = time.split(":")[1]
+    #     second = time.split(":")[2]
 
-if testbool:
-    print("Tadaaa")
-else:
-    print("sadface:(")
+    #     formatTime = time.struct_time(year, month, day, hour, minute, second)
 
-testrange = range(-2, 10)
-print(testrange)
+    #     return time.mktime(formatTime)
 
-for i in testrange:
-    print(i, end=" ")
+class Person:
+    def __init__(self, name, email, schoolclass, time, preferences):
+        self.mName = name
+        self.mEmail = email
+        self.mSchoolClass = schoolclass
+        self.mTime = time
+        self.mPreferences = preferences
+    def __str__(self):
+        return " ".join([self.mName, self.mEmail, self.mSchoolClass, str(self.mTime), str(self.mPreferences)])
+
+class Sorter:
+    def __init__(self):
+        self.mTest = "AhoJ svete"
+        pass
+
+class ExcelReader:
+    header = {}
+    def __init__(self, configinfo):
+        self.mHeaderIsRead = False
+        self.mConfigForExcelRead = configinfo
+    def readHeader(source):
+        pass
+
+pers1 = Person("Petr", "pavpetr@fit.cvut.cz", "8.a", datetime.datetime(2022, 6, 23), ["Anna", "Zuzka", "Alzbeta"])
+
+print(pers1.mPreferences)
+print(pers1)
+
+excelsource = load_workbook("dialog_data.xlsx")
+ws = excelsource.active
+print(ws["C3"].value)
+print(type(ws["C3"].value))
