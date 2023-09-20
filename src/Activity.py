4172b4a65b6bd4547d6cb48cd30bee7e9bc7f05d
Filip Spelina <spelifil@fit.cvut.cz>
2023-09-20 20:24:37 +0800

    v2: activity capacity

--- src/Activity.py
+++ src/Activity.py
@@ -1,6 +1,18 @@
+import configparser
+
+
 class Activity:
-    def __init__(self, name, slotNum):
+    def __init__(self, name:str, slotNum:int, capacityConfig):
         self.mName = name
         self.mSlot = slotNum
+        if name in capacityConfig:
+            self.mCapacity = capacityConfig[name]
+        else:
+            print("BEWARE! Capacity information for '" + name +"' not located in config")
+            
+            try:
+                self.mCapacity = int(input("Please input the desired capacity:\n"))
+            except:
     def __str__(self) -> str:
-        return "(ACT" + str(self.mSlot) + ": " + self.mName + ")" 
+        # return self.mName
+        return "(ACT" + str(self.mSlot) + ": " + self.mName + " -> " + self.mCapacity + ")" 
