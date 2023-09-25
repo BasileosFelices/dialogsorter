e29a00b04a68d9a04de0cda8b417292cf3bed852
Filip Spelina <spelifil@fit.cvut.cz>
2023-09-25 16:51:06 +0800

    v2: textmodeler documentation and reformat

--- src/TextModeler.py
+++ src/TextModeler.py
@@ -1,9 +1,26 @@
 class TextModeler:
+    """Helper class with static methods used for custom string handling"""
+
     def __new__(cls):
-        raise TypeError("TextModeler is a static class used for holding functions for string editing. It cannot be instanced")
-    
+        """instances of TextModeler are forbidden
+
+        Raises:
+            TypeError: during instantce making -> forbidden
+        """
+        raise TypeError(
+            "TextModeler is a static class used for holding functions for string editing. It cannot be instanced"
+        )
+
     @staticmethod
-    def cleanSpaces(string):
+    def cleanSpaces(string: str) -> str:
+        """Clears spaces before and after the string and multiple following spaces
+
+        Args:
+            string (str): original string
+
+        Returns:
+            str: spaces cleaned string
+        """
         if not isinstance(string, str):
             return string
         return " ".join(string.split())
