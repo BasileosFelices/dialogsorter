49b57238529733be6027295f20ca03ebb6520b7d
Filip Spelina <spelifil@fit.cvut.cz>
2023-09-25 10:30:42 +0800

    v2: Activity documentation and refactoring

--- src/Activity.py
+++ src/Activity.py
@@ -1,20 +1,49 @@
-import configparser
-from email.errors import ObsoleteHeaderDefect
-from types import NoneType
+import configparser as cp
+
+from Person import Person
 
 
 class Activity:
-    def __init__(self, name:str, slotNum:int, capacityConfig = None):
+    """Activity class for holding a lecture information as well as attendance list
+
+    Attributes:
+        mName (str): required attribute, used for searching, activities with same name are considered eq
+        mSlot (int): slot number the activity belongs to
+        mCapacity (int): maximum number or attendants
+        mApplicants (list): list of people asigned to the activity
+    """
+
+    mName = str()
+    mSlot = int()
+    mCapacity = int()
+    mApplicants = list()
+
+    def __init__(
+        self, name: str, slotNum: int, capacityConfig: cp.ConfigParser = None
+    ) -> None:
+        """constructs an Activity, without passing capacityconfig, the capacity is set to 0
+
+        This behaviour is mainly for creating temporary activities used only for searching or sorting
+
+        Args:
+            name (str): Required parameter, used for searching, activities with same name are considered eq
+            slotNum (int): slot number the activity belongs to
+            capacityConfig (cp.ConfigParser, optional): configfile with view of ["capacity"], for loading capacity. Defaults to None.
+        """
+
         self.mName = name
         self.mSlot = slotNum
         self.mApplicants = []
         if bool(capacityConfig) is False:
+            self.mCapacity = 0
             return
         if name in capacityConfig:
             self.mCapacity = capacityConfig.getint(name)
         else:
-            print("BEWARE! Capacity information for '" + name +"' not located in config")
-            
+            print(
+                "BEWARE! Capacity information for '" + name + "' not located in config"
+            )
+
             inputvalid = False
             while not inputvalid:
                 try:
@@ -23,17 +52,52 @@ class Activity:
                     print("Invalid input, please try again.")
                 else:
                     inputvalid = True
+
     def __str__(self) -> str:
-        # return self.mName
-        return "(ACT" + str(self.mSlot) + ": " + self.mName + " -> " + str(self.mCapacity) + ")" 
-    
+        """debug printing settings for activity class
+
+        Returns:
+            str: slot, name and capacity information
+        """
+
+        return (
+            "(ACT"
+            + str(self.mSlot)
+            + ": "
+            + self.mName
+            + " -> "
+            + str(self.mCapacity)
+            + ")"
+        )
+
     def __eq__(self, other) -> bool:
+        """compares equality of Activity based on name
+
+        Returns:
+            bool: True if equal names
+        """
+
         return True if self.mName == other.mName else False
 
-    def addPerson(self, person) -> bool:
+    def __lt__(self, other) -> bool:
+        """compares two Activity based on name
+
+        Returns:
+            bool: True if name is less
+        """
+
+        return True if self.mName < other.mName else False
+
+    def addPerson(self, person: Person) -> bool:
+        """Adds a person to the attendance list if the capacity is not filled
+
+        Args:
+            person (Person): Person to be added to attendance list, Person class expected
+
+        Returns:
+            bool: True if applicant is written to list, False if the activity is full
+        """
         if len(self.mApplicants) >= self.mCapacity:
             return False
         self.mApplicants.append(person)
         return True
-        
-        
